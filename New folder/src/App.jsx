263b7fc194593fe9cd82b511fb9e025a2271f1f3
Yogesh  Kumar soni <153350265+bcacoders2023@users.noreply.github.com>
2024-08-14 15:27:36 +0530

    Add files via upload

---
+++ New folder/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-slate-500">
+      Welcome To the React+Vite with Tailwind Starter Pack
+    </h1>
+    </>
+  )
+}
+
+export default App
